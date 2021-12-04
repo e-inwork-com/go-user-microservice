0902febe18da4a6609211b9e13cf5e8b2acf1da3
Taufik Hidayat <t.hidayat.nl@gmail.com>
2021-12-04 17:24:07 +0100

    Format code style

--- pkg/data/users.go
+++ pkg/data/users.go
@@ -21,10 +21,10 @@ var AnonymousUser = &User{}
 type User struct {
 	ID        uuid.UUID	`json:"id"`
 	CreatedAt time.Time `json:"created_at"`
-	Name      string    `json:"name"`
-	Email     string   `json:"email"`
-	Password  password `json:"-"`
-	Activated bool     `json:"activated"`
+	Name      string   	`json:"name"`
+	Email     string   	`json:"email"`
+	Password  password 	`json:"-"`
+	Activated bool     	`json:"activated"`
 	Version   int       `json:"-"`
 }
 
