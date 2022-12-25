936e25f58aaddbbac1bbde3f694b6c19710e4fe7
Taufik Hidayat <t.hidayat.nl@gmail.com>
2022-12-25 04:37:27 +0100

    EGOUS-3: Update end to end unit testing

--- api/routes_test.go
+++ api/routes_test.go
@@ -2,29 +2,82 @@ package api
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/e-inwork-com/go-user-service/pkg/data"
+	"github.com/e-inwork-com/go-user-service/pkg/jsonlog"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
+	"time"
 
-	"github.com/e-inwork-com/go-user-service/pkg/data"
-	"github.com/e-inwork-com/go-user-service/pkg/jsonlog"
-
-	"github.com/cockroachdb/cockroach-go/v2/testserver"
 	"github.com/stretchr/testify/assert"
+
+	_ "github.com/lib/pq"
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
+	log "github.com/sirupsen/logrus"
 )
 
-func TestRoutes(t *testing.T) {
-	// Server Setup
-	tsDB, err := testserver.NewTestServer()
-	assert.Nil(t, err)
-	urlDB := tsDB.PGURL()
+var db *sql.DB
 
+func TestRoutes(t *testing.T) {
+	// uses a sensible default on windows (tcp/http) and linux/osx (socket)
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not construct pool: %s", err)
+	}
+
+	err = pool.Client.Ping()
+	if err != nil {
+		log.Fatalf("Could not connect to Docker: %s", err)
+	}
+
+	// pulls an image, creates a container based on it and runs it
+	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "11",
+		Env: []string{
+			"POSTGRES_PASSWORD=postgres",
+			"POSTGRES_USER=postgres",
+			"POSTGRES_DB=postgres",
+			"listen_addresses = '*'",
+		},
+	}, func(config *docker.HostConfig) {
+		// set AutoRemove to true so that stopped container goes away by itself
+		config.AutoRemove = true
+		config.RestartPolicy = docker.RestartPolicy{Name: "no"}
+	})
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	hostAndPort := resource.GetHostPort("5432/tcp")
+	databaseUrl := fmt.Sprintf("postgres://postgres:postgres@%s/postgres?sslmode=disable", hostAndPort)
+
+	log.Println("Connecting to database on url: ", databaseUrl)
+
+	// Tell docker to hard kill the container in 120 seconds
+	resource.Expire(120)
+
+	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
+	pool.MaxWait = 120 * time.Second
+	if err = pool.Retry(func() error {
+		db, err = sql.Open("postgres", databaseUrl)
+		if err != nil {
+			return err
+		}
+		return db.Ping()
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	// Server Setup
 	var cfg Config
-	cfg.Db.Dsn = urlDB.String()
+	cfg.Db.Dsn = databaseUrl
 	cfg.Auth.Secret = "secret"
 	cfg.Db.MaxOpenConn = 25
 	cfg.Db.MaxIdleConn = 25
@@ -40,6 +93,7 @@ func TestRoutes(t *testing.T) {
 	defer db.Close()
 
 	_, err = db.Exec("" +
+		"CREATE EXTENSION IF NOT EXISTS pgcrypto;" +
 		"CREATE TABLE IF NOT EXISTS users (" +
 		"id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid()," +
 		"created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()," +
